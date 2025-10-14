4794177855d8f66f85268da7a78dc605aac92336
fahrudina <fahrudinjaya@gmail.com>
2025-10-14 13:34:30 +0700

    Fix replit.nix file - remove mixed TOML content

--- replit.nix
+++ replit.nix
@@ -2,47 +2,5 @@
   deps = [
     pkgs.nodejs-18_x
     pkgs.nodePackages.npm
-    pkgs.nodePackages.yarn
   ];
 }
-
-[gitHubImport]
-requiredFiles = [".replit", "replit.nix", ".config"]
-
-[packager]
-language = "nodejs"
-
-  [packager.features]
-  packageSearch = true
-  guessImports = true
-  enabledForHosting = false
-
-[unitTest]
-language = "nodejs"
-
-[debugger]
-support = true
-
-  [debugger.interactive]
-  transport = "localhost:0"
-  startCommand = ["dap-node"]
-
-    [debugger.interactive.initializeMessage]
-    command = "initialize"
-    type = "request"
-
-    [debugger.interactive.launchMessage]
-    command = "launch"
-    type = "request"
-    
-      [debugger.interactive.launchMessage.arguments]
-      pauseForSourceMap = false
-      args = []
-      cwd = "."
-      environment = []
-      program = "."
-      request = "launch"
-      type = "pwa-node"
-      sourceMaps = true
-      skipFiles = ["<node_internals>/**"]
-      console = "externalTerminal"
